b2356b562901fd7ef72a2b12c7442ac2065c8c60
mushastudio <mrmusha2021@gmail.com>
2026-02-02 23:00:42 +0530

    first commit

---
+++ src/main/java/com/example/smartBiz/SmartBizApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartBiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartBiz/SmartBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartBiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
